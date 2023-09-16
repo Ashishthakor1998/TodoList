f9ddc5955d4c943dcdcc9dfa0c14e0eeaf75d526
Ashishthakor1998 <143682249+Ashishthakor1998@users.noreply.github.com>
2023-09-16 14:38:46 +0530

    Adding unit test
    for test edit, cancel and complete task should working properly or not

--- todo-list-app/src/app/task-list/task-list.component.spec.ts
+++ todo-list-app/src/app/task-list/task-list.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { TaskListComponent } from './task-list.component';
 
@@ -8,14 +9,42 @@ describe('TaskListComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TaskListComponent]
+      declarations: [TaskListComponent],
+      imports: [FormsModule],
     });
     fixture = TestBed.createComponent(TaskListComponent);
     component = fixture.componentInstance;
+    component.tasks = [
+      { id: 1, title: 'Task 1', completed: false },
+      { id: 2, title: 'Task 2', completed: true },
+    ];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+    it('should edit a task', () => {
+    const taskToEdit = component.tasks[0];
+    component.editTask(taskToEdit);
+    component.editingTask.title = 'Edited Task';
+    component.saveEditedTask();
+    expect(taskToEdit.title).toBe('Edited Task');
+  });
+
+  it('should cancel editing a task', () => {
+    const taskToEdit = component.tasks[0];
+    component.editTask(taskToEdit);
+    component.editingTask.title = 'Edited Task';
+    component.cancelEdit();
+    expect(taskToEdit.title).not.toBe('Edited Task');
+  });
+
+  it('should complete a task', () => {
+    const taskToComplete = component.tasks[0];
+    component.completeTask(taskToComplete);
+    expect(taskToComplete.completed).toBe(true);
+  });
+
 });
