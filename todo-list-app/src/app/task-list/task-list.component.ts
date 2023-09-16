4ebbd334956fd0104f5182d31c25948b7d2048fc
Ashishthakor1998 <143682249+Ashishthakor1998@users.noreply.github.com>
2023-09-16 14:47:04 +0530

    Adding originalEditingTaskTitle
    you store the original title of the task in the originalEditingTaskTitle variable.

--- todo-list-app/src/app/task-list/task-list.component.ts
+++ todo-list-app/src/app/task-list/task-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Task } from '../task.model';
 import { TaskService } from '../task.service';
-
+// import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
@@ -10,7 +10,8 @@ export class TaskListComponent {
   tasks: Task[] = [];
   newTask: Task = new Task();
   editingTask: any;
-
+  originalEditingTaskTitle!: string; // Store the original task title
+  
   constructor(private taskService: TaskService) {
     this.tasks = this.taskService.getTasks();
   }
@@ -28,6 +29,7 @@ export class TaskListComponent {
 
   editTask(task: Task): void {
     this.editingTask = task;
+    this.originalEditingTaskTitle = task.title; // Store original title
   }
 
   saveEditedTask() {
@@ -38,6 +40,8 @@ export class TaskListComponent {
   }
 
   cancelEdit() {
+    this.editingTask.title = this.originalEditingTaskTitle; // Revert to the original title
+    this.originalEditingTaskTitle = ''; // Clear original title
     this.editingTask = null;
   }
 
